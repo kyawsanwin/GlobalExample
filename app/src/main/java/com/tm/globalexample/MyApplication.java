9723303dd7621f490f4e392c8a3c5e06426d246e
Kyaw San Win <zues88@gmail.com>
2015-07-03 10:40:26 +0630

    Create MyApplication class for global variables. In this case, using for typeface.

---
+++ app/src/main/java/com/tm/globalexample/MyApplication.java
@@ -0,0 +1,23 @@
+package com.tm.globalexample;
+
+import android.app.Application;
+import android.content.Context;
+import android.graphics.Typeface;
+
+/**
+ * Created by kyawsanwin on 7/3/15.
+ */
+public class MyApplication extends Application {
+    Context context;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = this;
+    }
+
+    public Typeface getTypeface(Context context) {
+        Typeface typeface = Typeface.createFromAsset(context.getAssets(),
+                "fonts/latobold.ttf");
+        return typeface;
+    }
+}
